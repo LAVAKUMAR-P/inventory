ef2597a2a5bcd55e8fe51427d210fe3553a19deb
LAVAKUMAR-P <84187714+LAVAKUMAR-P@users.noreply.github.com>
2021-12-14 14:24:37 +0530

    api link updated

--- src/components/settings.js
+++ src/components/settings.js
@@ -1,7 +1,7 @@
 let getSettings = () => {
     return {
         api : "http://localhost:3001",
-        // api : "https://shopnode123.herokuapp.com",
+        // api : "https://inventorynode.herokuapp.com",
     }
 }
 
